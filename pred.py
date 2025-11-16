669d6e9cf8faadfa1df1c87290a3bd9e8e0ec786
Jackie <jackie.jq.chen@gmail.com>
2025-11-15 20:47:09 -0500

    Data processing functions upload（Version1

--- pred.py
+++ pred.py
@@ -32,43 +32,46 @@ def parse_multiselect(response):
         return []
     return [s.strip() for s in str(response).split(",") if s.strip()]
 
-def build_data_matrix(df: pd.DataFrame) -> list[pd.DataFrame]:
+def build_data_matrix(df: pd.DataFrame) -> np.ndarray:
     """
     Construct data Matrix from dataframe based on three types of features:
     free texts, ratings, multiplicative questions
     """
-    text_cols = [1, 6, 9]
-    rating_cols = [2, 4, 7, 8]
-    multi_cols = [3, 5]
+    text_cols = [1, 6, 9] # the generative questions columns
+    rating_cols = [2, 4, 7, 8] # the rating questions columns
+    multi_cols = [3, 5] # the multiplicative choices questions columns
 
     cols = df.columns
 
+    # generate a text vocabulary list
     free_texts = []
     for i in text_cols:
         col = cols[i]
         word_counts = (df[col].fillna("").astype(str).apply(lambda s: len(s.split())))
-        words = word_counts.to_numpy(dtype=int)
+        words = word_counts.to_numpy(dtype=float)
         free_texts.append(words)
 
+    # generate the list of rating integers
     ratings = []
     for i in rating_cols:
         col = cols[i]
 
+        # if there is a missing value, fill with the mode value or 0 if the mode value does
+        # not exist
         original_lst = df[col].apply(extract_rating)
-        if not original_lst.mode().empty:
-            mode_value = original_lst.mode().iloc[0]
-        else:
-            mode_value = 0
+        modes = original_lst.mode()
+        mode_value = modes.iloc[0] if not modes.empty else 0
 
         nums = original_lst.fillna(mode_value).astype(float)
         nums = nums.to_numpy()
         ratings.append(nums)
 
+    # generate multi_cols based on the number of answers
     multiplicative_questions = []
     for i in multi_cols:
         col = cols[i]
-        counts = (df[col].apply(parse_multiselect).apply(len).astype(float))
-        counts = counts.to_numpy()
+        counts_answers = (df[col].apply(parse_multiselect).apply(len).astype(float))
+        counts = counts_answers.to_numpy()
         multiplicative_questions.append(counts)
 
     x_all = free_texts + ratings + multiplicative_questions
@@ -85,7 +88,7 @@ def main():
     print(y)
     print(x_matrix[0])
     print(y[0])
-    print(x_matrix.shape)
+    print("The shape of data X matrix", x_matrix.shape)
     print(y.shape)
 
     np.save("x_matrix.npy", x_matrix)
